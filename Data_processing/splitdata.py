8a3c8f060102bb49c0c4736242e46b46c043cb37
Giap-Van <56864229+Giap-Van@users.noreply.github.com>
2021-11-09 16:38:04 +0700

    fix data suffle

--- Data_processing/splitdata.py
+++ Data_processing/splitdata.py
@@ -4,7 +4,13 @@ import os
 data = pd.read_csv('/storage/vangt/TTS/female/metadata.csv', sep='|', header=None)
 
 # Suffle
-data = data.sample(frac=1)
+data = data.sample(frac=1).reset_index(drop=True)
+
+#for i in range(10):
+#    print(data[0][i], ' ', data[1][i])
+
+#print(data.shape)
+#print(data[0:10])
 
 train_ratio = 0.8
 train_index = int(train_ratio * len(data))
@@ -12,6 +18,8 @@ train_index = int(train_ratio * len(data))
 with open('/storage/vangt/TTS/training.txt', 'w') as fd:
     for i, fname in enumerate(data[0][:train_index]):
         PATH = os.path.join(os.getcwd(), 'female/wavs')
+        #if i < 10:
+        #    print(fname,' ', data[1][i])
         fd.write('{}|{}\n'.format(os.path.join(PATH, fname), data[1][i]))
 
 with open('/storage/vangt/TTS/testing.txt', 'w') as fd:
